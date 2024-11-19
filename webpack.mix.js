d85b0afc76c29f93fe4e7268d51e29f78041bd0e
JODHI <jodhi.gunawan@salt.co.id>
2024-11-19 10:41:07 +0700

    update starter

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+require('laravel-mix-purgecss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass("resources/assets/scss/main-style.scss", "public/css")
+    .js('resources/assets/js/main-script.js', 'public/js')
+    .purgeCss({ enabled: true })
+    .options({ autoprefixer: { remove: false } });
